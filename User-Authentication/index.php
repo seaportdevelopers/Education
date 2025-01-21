922c38d0bd364811e75287e212f279a443087a1b
227-MT-SK <@>
2025-01-21 20:00:55 +0200

    Code updates

--- User-Authentication/index.php
+++ User-Authentication/index.php
@@ -3,8 +3,10 @@ include('db_connect.php');
 
 if (isset($_POST['submit'])) {
     $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
     $postData = [
-        'firstName' => $_POST['firstName']
+        'firstName' => $_POST['firstName'],
+        'lastName' => $_POST['lastName']
     ];
 
     $ref_table = "contacts";
@@ -13,7 +15,7 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -27,6 +29,8 @@ if (isset($_POST['submit'])) {
 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <label for="first_name">First Name:</label>
     <input type="text" name="firstName" id="first_name" placeholder="First Name" required>
+    <label for="last_name">Last Name:</label>
+    <input type="text" name="lastName" id="last_name" placeholder="Last Name" required>
     <input type="submit" name="submit" value="Submit">
 </form>
 
