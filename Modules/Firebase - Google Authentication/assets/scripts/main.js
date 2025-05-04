a8d1088ce90a9c33290274aec782737752f3fbc8
PIOTR CYBULSKIJ <info@piotr.lt>
2025-05-04 17:43:23 +0300

    File structure updates

--- Modules/Firebase - Google Authentication/assets/scripts/main.js
+++ Modules/Firebase - Google Authentication/assets/scripts/main.js
@@ -3,13 +3,13 @@ import {getAuth, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.
 import {getAnalytics} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyABMSFxdtfT_set9rUDhcWVLMW5fYxYVyA",
-    authDomain: "authentication-e4c8c.firebaseapp.com",
-    projectId: "authentication-e4c8c",
-    storageBucket: "authentication-e4c8c.firebasestorage.app",
-    messagingSenderId: "580479540930",
-    appId: "1:580479540930:web:d0d0b76869873d8d25655b",
-    measurementId: "G-JQ17FWCCSY"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
